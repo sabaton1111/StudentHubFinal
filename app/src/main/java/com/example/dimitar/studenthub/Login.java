4691a7964bbe97822c43bcc5e31ecf788d722535
sabaton1111 <dimiter41@gmail.com>
2016-02-13 21:41:50 +0200

    Update

--- app/src/main/java/com/example/dimitar/studenthub/Login.java
+++ app/src/main/java/com/example/dimitar/studenthub/Login.java
@@ -1,7 +1,10 @@
 package com.example.dimitar.studenthub;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Login extends AppCompatActivity {
 
@@ -9,5 +12,18 @@ public class Login extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        onClickListener();
+    }
+
+    public void onClickListener()
+    {
+        Button buttonSignUp = (Button)findViewById(R.id.buttonSignUp);
+        buttonSignUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intentSignUp = new Intent("com.example.dimitar.studenthub.SignUp");
+                startActivity(intentSignUp);
+            }
+        });
     }
 }
