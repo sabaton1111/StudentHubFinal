d67cb281fef905a6bd2cfed8b134714b59bb2efe
sabaton1111 <dimiter41@gmail.com>
2016-02-14 14:56:29 +0200

    Update

--- app/src/main/java/com/example/dimitar/studenthub/NamesFragment.java
+++ app/src/main/java/com/example/dimitar/studenthub/NamesFragment.java
@@ -1,5 +1,7 @@
 package com.example.dimitar.studenthub;
 
+import android.app.FragmentManager;
+import android.app.FragmentTransaction;
 import android.content.Context;
 import android.net.Uri;
 import android.os.Bundle;
@@ -7,16 +9,36 @@ import android.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 
 public class NamesFragment extends Fragment {
-
+View view;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_names, container, false);
+        view= inflater.inflate(R.layout.fragment_names, container, false);
+        Button button4 = (Button)view.findViewById(R.id.buttonNext3);
+        if (container != null) {
+            container.removeAllViews();
+        }
+        button4.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+
+                Fragment fragment4;
+                fragment4 = new LastFragment();
+                FragmentManager fm = getFragmentManager();
+                FragmentTransaction ft = fm.beginTransaction();
+                ft.replace(R.id.parent_fragment,fragment4);
+                ft.commit();
+            }
+        });
+        return view;
     }
 
 
 }
+
