e094718d68be5dab3a484e2e0d1294e63e334a62
MartinDachev <martin.dachev@hotmail.com>
2016-06-05 00:43:07 +0300

    webview za mitaka

---
+++ app/src/main/java/com/mdsolutions/dimitar/studenthub/WebViewActivity.java
@@ -0,0 +1,38 @@
+package com.mdsolutions.dimitar.studenthub;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.Toast;
+
+public class WebViewActivity extends AppCompatActivity {
+
+    WebView mWebview;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        mWebview  = new WebView(this);
+
+        mWebview.getSettings().setJavaScriptEnabled(true); // enable javascript
+
+        final Activity activity = this;
+
+        mWebview.setWebViewClient(new WebViewClient() {
+            public void onReceivedError(WebView view, int errorCode, String description, String failingUrl) {
+                Toast.makeText(activity, description, Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        mWebview.loadUrl("http://eruditio.ml/");
+        setContentView(mWebview);
+    }
+
+}
